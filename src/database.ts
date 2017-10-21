63bd44ad87aaf887bd3d790c026e25e57b247ca0
jakoblorz <jakob@lorz.org>
2017-10-21 17:33:11 +0200

    implemented a method to read a list of tuples

--- src/database.ts
+++ src/database.ts
@@ -24,10 +24,10 @@ export class Database<T extends {}, R extends string> {
      * @param relation specify on which relation the operation should happen
      * @param where specify the constraints a tuple needs to fullfill to be selected
      * @param select specify the columns that should be returned - should be ALL and the
-     * SAME as the keys defined in the Y type
+     * SAME as the keys defined in the X type
      */
     public async readResource<X extends T, Y extends T>(
-        relation: R, where: Y, select: Array<keyof T> | keyof T | "*" = "*"): Promise<X | null> {
+        relation: R, where: Y, select: Array<keyof X> | keyof X | "*" = "*"): Promise<X | null> {
             
             // reduce the where clause into accessible structure
             const { clause, values } = await this.where<Y>(where);
@@ -41,6 +41,61 @@ export class Database<T extends {}, R extends string> {
             // execute the query
             return await connection.oneOrNone(
                 "SELECT " + columns + " FROM " + relation + " WHERE " + clause + " LIMIT 1", values) || null;
+        }
+
+    /**
+     * readResourceList
+     * @param relation specify on which relation the operation should happen
+     * @param where specify the constraints a tuple needs to fullfill to be selected
+     * @param select specify the columns that should be retured - should be ALL and the
+     * SAME as the keys defined in the X type
+     * @param skip specify the tuples that should be skipped before selecting - NOTE: make
+     * use of the orderByAsc/orderByDsc arguments to give sense to the expression 'skip the
+     * first x tuples'
+     * @param limit specify a maximum count of tuples returned
+     * @param orderByAsc specify columns which should be ordered in ascending order
+     * @param orderByDsc specify columns which should be ordered in descending order
+     */
+    public async readResourceList<X extends T, Y extends T>(
+        relation: R, where: Y, select: Array<keyof X> | keyof X | "*" = "*",
+        skip: number = 0, limit: number = 10,
+        orderByAsc: Array<keyof X> | keyof X | string = "",
+        orderByDsc: Array<keyof X> | keyof X | string = "") {
+            
+            // reduce the where clause into accessible structure
+            const { clause, values } = await this.where<Y>(where);
+
+            // build string of selected columns
+            const columns = typeof select === "string" ? select : select.toString();
+
+            //  build ORDER BY data for ascending orders
+            const ascending = typeof orderByAsc === "string" ? orderByAsc + " ASC " : (orderByAsc as string[])
+                .reduce<string>((acc, val) => acc += val + " ASC, ", "").slice(0, -2);
+
+            // build ORDER BY data for descending orders
+            const descending = typeof orderByDsc === "string" ? orderByDsc + " DESC " : (orderByAsc as string[])
+                .reduce<string>((acc, val) => acc += val + " DESC, ", "").slice(0, -2);
+
+            // check if ORDER BY statement is wanted
+            const useOrderBy = (orderByAsc === "" ? undefined : "" || orderByDsc === "" ? undefined : "") !== undefined;
+
+            // get the database interface
+            const connection = await this.connect();
+
+            // execute the query
+            return await connection.many(
+                // SELECT a,b,c FROM relation WHERE a = $1
+                "SELECT " + columns + " FROM " + relation + " WHERE " + clause +
+
+                // ORDER BY a ASC, b DSC
+                (useOrderBy ? " ORDER BY " : "") +
+                // insert ASC statements, check if a comma is required at the end because there
+                // will be DSC clauses
+                (orderByAsc !== "" ? (orderByDsc !== "" ? ascending + ", " : ascending) : "") +
+                (orderByDsc !== "" ? descending : "") +
+
+                // OFFSET 10 LIMIT 10
+                (skip > 0 ? " OFFSET " + skip + " " : "") + (limit > 0 ? " LIMIT " + limit + " " : ""), values);
         }
 
     /**
